9b40b9182b650644c6b3f3571883c1766de91ad7
shogo1229 <neko2000shogo@gmail.com>
2023-06-13 14:35:29 +0900

    06/13 add comment

--- train_Spatial.py
+++ train_Spatial.py
@@ -19,103 +19,109 @@ from Network.Spatial.VGG16 import VGG16_Spatial
 from Network.Spatial.GhostNet import Yuda_GhostNet
 from Network.Spatial.MobileNet import MobileNet_V2_Spatial, MobileNet_V3_small_Spatial,MobileNet_V3_large_Spatial
 from sklearn.model_selection import train_test_split
-
-EpochNum = 21                                          
+#-----------------------------ハイパラ等の宣言-----------------------------#
+EpochNum = 100                                          
 Height = 224                                            
 Width = 224                                           
 BatchSize = 16
-LearningRate = 0.001                           
-DatasetPath = r"F:\AROB_Journal\Normal_MHI_log5\train\RGB" 
-valDatasetPath =r"F:\AROB_Journal\Normal_MHI_log5\val\RGB"
-modelPath = r"MobileNet_RGB_AROB-Journal"          
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")       
+LearningRate = 0.001 
+#----------------------データセット、モデルパスの宣言-------------------------#
+
+DatasetPath = r""  # データセットのパス
+valDatasetPath = r""  # 検証データセットのパス
+modelPath = r""  # モデルの保存先パス
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")  # 使用するデバイスの指定 (GPUが利用可能ならばGPUを使用)
 
 
-
+#----------------------学習用コード-------------------------#
 class Trainer():
     def __init__(self,model, optimizer, criterion, trainLoader, valLoader, transform):
-        self.model = model
-        self.optimizer = optimizer
-        self.criterion = criterion
-        self.trainLoader = trainLoader
-        self.valLoader = valLoader
-        self.transform = transform
-        self.totalTrainLoss = []
-        self.TrainCorrect = []
-        self.totalValLoss = []
-        self.ValCorrect = []
-        self.Fig = plt.figure(figsize=[10,10])
-        self.max_acc = 0.0
-        self.min_loss = 100
-    def Train(self,epoch):
-        self.model.train()
-        train_loss,train_acc = 0.0,0.0
-        t_loss,t_acc =  0.0,0.0
+        self.model = model  # モデル
+        self.optimizer = optimizer  # 最適化アルゴリズム
+        self.criterion = criterion  # 損失関数
+        self.trainLoader = trainLoader  # 訓練データのデータローダー
+        self.valLoader = valLoader  # 検証データのデータローダー
+        self.transform = transform  # データの前処理
+        self.totalTrainLoss = []  # 訓練データの損失
+        self.TrainCorrect = []  # 訓練データの正解率
+        self.totalValLoss = []  # 検証データの損失
+        self.ValCorrect = []  # 検証データの正解率
+        self.Fig = plt.figure(figsize=[10,10])  # グラフの描画領域
+        self.max_acc = 0.0  # 最高正解率
+        self.min_loss = 100  # 最小損失
+
+    def Train(self, epoch):
+        self.model.train()  # モデルを訓練モードに設定
+        train_loss, train_acc = 0.0, 0.0
+        t_loss, t_acc = 0.0, 0.0
         train_log = ""
-        for batchIdx,(img,label) in enumerate(self.trainLoader):
-            img,label = Variable(img.cuda()),Variable(label.cuda())
-            output = self.model(img)
-            loss = self.criterion(output,label)
+        for batchIdx, (img, label) in enumerate(self.trainLoader):
+            img, label = Variable(img.cuda()), Variable(label.cuda())
+            output = self.model(img)  # モデルにデータを入力し、予測値を取得
+            loss = self.criterion(output, label)  # 予測値と正解ラベルの損失を計算
             train_loss += loss.data.item()
-            self.optimizer.zero_grad()
-            loss.backward()     
-            self.optimizer.step()
-            pred = output.data.max(dim=1)[1]
-            train_acc += pred.eq(label.data).cpu().sum()
-            t_loss = train_loss/((batchIdx+1)*BatchSize)
-            t_acc  = 100*train_acc.data.item() / ((batchIdx+1)*BatchSize)
+            self.optimizer.zero_grad()  # 勾配を初期化
+            loss.backward()  # 逆伝播を実行
+            self.optimizer.step()  # パラメータの更新
+            pred = output.data.max(dim=1)[1]  # 最も確率の高いクラスを予測
+            train_acc += pred.eq(label.data).cpu().sum()  # 正解数をカウント
+            t_loss = train_loss / ((batchIdx+1) * BatchSize)  # 平均訓練損失を計算
+            t_acc = 100 * train_acc.data.item() / ((batchIdx+1) * BatchSize)  # 正解率を計算
             train_log = "epoch : {:3} train_loss : {:3.12} train_acc : {:3.12}".format(str(epoch+1), str(t_loss), str(t_acc))
-            print("\r"+train_log,end="")
+            print("\r"+train_log, end="")
         self.totalTrainLoss.append(t_loss)
         self.TrainCorrect.append(t_acc)
-        self.model.eval()
-        val_loss,val_acc = 0.0,0.0
-        v_loss,v_acc = 0.0,0.0
+
+        self.model.eval()  # モデルを評価モードに設定
+        val_loss, val_acc = 0.0, 0.0
+        v_loss, v_acc = 0.0, 0.0
         val_log = ""
         with torch.no_grad():
-            for batchIdx,(img,label) in enumerate(self.valLoader):
-                img,label = Variable(img.cuda()),Variable(label.cuda())
-                output = self.model(img)
-                loss = self.criterion(output,label)
+            for batchIdx, (img, label) in enumerate(self.valLoader):
+                img, label = Variable(img.cuda()), Variable(label.cuda())
+                output = self.model(img)  # モデルにデータを入力し、予測値を取得
+                loss = self.criterion(output, label)  # 予測値と正解ラベルの損失を計算
                 val_loss += loss.data.item()
-                pred = output.data.max(dim=1)[1]
-                val_acc += pred.eq(label.data).cpu().sum()
-                v_loss = val_loss/((batchIdx+1)*BatchSize)
-                v_acc = 100*val_acc.data.item() / ((batchIdx+1)*BatchSize)
+                pred = output.data.max(dim=1)[1]  # 最も確率の高いクラスを予測
+                val_acc += pred.eq(label.data).cpu().sum()  # 正解数をカウント
+                v_loss = val_loss / ((batchIdx+1) * BatchSize)  # 平均検証損失を計算
+                v_acc = 100 * val_acc.data.item() / ((batchIdx+1) * BatchSize)  # 正解率を計算
                 val_log = train_log + " val_loss : {:3.9} val_acc : {:3.9}".format(str(v_loss), str(v_acc))
-                print("\r"+val_log,end="")
+                print("\r"+val_log, end="")
             self.totalValLoss.append(v_loss)
             self.ValCorrect.append(v_acc)
         print()
         if v_acc > self.max_acc:
             self.max_acc = v_acc
+            # 最高正解率を達成した場合にモデルを保存
             with open("models/"+str(modelPath)+"/"+str(modelPath)+"_max-Acc.pth", "wb") as savePath:
                 torch.save({'epoch': epoch,
                             'model_state_dict': model.state_dict(),
                             'optimizer_state_dict': optimizer.state_dict(),
-                            'loss': loss,},savePath)
+                            'loss': loss}, savePath)
         if v_loss < self.min_loss:
             self.min_loss = v_loss
+            # 最小損失を達成した場合にモデルを保存
             with open("models/"+str(modelPath)+"/"+str(modelPath)+"_min-Loss.pth", "wb") as savePath:
                 torch.save({'epoch': epoch,
                             'model_state_dict': model.state_dict(),
                             'optimizer_state_dict': optimizer.state_dict(),
-                            'loss': loss,},savePath)
+                            'loss': loss}, savePath)
 
     def graph_plot(self):
-        plt.clf()
+        plt.clf()  # グラフをクリア
         plt.style.use('ggplot')
-        lossFig = self.Fig.add_subplot(2,1,1) 
+        lossFig = self.Fig.add_subplot(2, 1, 1) 
         plt.title('Loss Graph')
         plt.plot(self.totalTrainLoss, label='train loss')
         plt.plot(self.totalValLoss, label='validation loss')
         plt.xlabel('Epoch')
         plt.ylabel('Loss')
         plt.legend()
-        accuracyFig = self.Fig.add_subplot(2,1,2) 
-        plt.title('Accuracy Graph (max_Val-acc :'+str(round(float(self.max_acc),4))+'%)')
+        accuracyFig = self.Fig.add_subplot(2, 1, 2) 
+        plt.title('Accuracy Graph (max_Val-acc :'+str(round(float(self.max_acc), 4))+'%)')
         plt.minorticks_on()
-        plt.ylim(0,100)
+        plt.ylim(0, 100)
         plt.plot(self.TrainCorrect, label='train acc')
         plt.plot(self.ValCorrect, label='validation acc')
         plt.xlabel('Epoch')
@@ -139,19 +145,16 @@ if __name__ == '__main__':
     
     os.mkdir("Models/"+str(modelPath))
     criterion = nn.CrossEntropyLoss()
-    train_dataset = datasets.ImageFolder(root=DatasetPath, transform=transform)
-    val_dataset = datasets.ImageFolder(root=valDatasetPath, transform=transform)
+    train_dataset = datasets.ImageFolder(root=DatasetPath, transform=transform)  # 訓練データセットの読み込み
+    val_dataset = datasets.ImageFolder(root=valDatasetPath, transform=transform)  # 検証データセットの読み込み
     print("Load DataSet 「"+str(DatasetPath)+"」")
-    #train_dataset, val_dataset, train_label, test_label = train_test_split(dataset, dataset_labels, test_size=0.2, random_state=100,shuffle=True
     trainLoader = torch.utils.data.DataLoader(train_dataset, batch_size=BatchSize, shuffle=True, num_workers=os.cpu_count(), pin_memory=True)
     valLoader = torch.utils.data.DataLoader(val_dataset, batch_size=BatchSize, shuffle=False, num_workers=os.cpu_count(), pin_memory=True)
-    print(len(train_dataset),len(val_dataset))
+    print(len(train_dataset), len(val_dataset))
     MobileNet = MobileNet_V2_Spatial()
-    model = MobileNet.model.cuda()
-    #os.mkdir("Models/"+str(modelPath))
+    model = MobileNet.model.cuda()  # モデルをGPUに転送
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters(),lr=LearningRate)
-    #train_dataset = Subset(dataset,train_dataset)
-    print("Learning rate :",LearningRate)
-    train = Trainer(model, optimizer, criterion,trainLoader, valLoader, transform)
+    optimizer = optim.Adam(model.parameters(), lr=LearningRate)  # Adam最適化アルゴリズムを使用
+    print("Learning rate :", LearningRate)
+    train = Trainer(model, optimizer, criterion, trainLoader, valLoader, transform)
     train.main()
--- train_Spatial_Resume.py
+++ train_Spatial_Resume.py
@@ -21,73 +21,80 @@ from Network.Spatial.MobileNet import MobileNet_V2_Spatial, MobileNet_V3_small_S
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import ShuffleSplit, StratifiedKFold
 
-EpochNum = 2000                                          
+#-----------------------------ハイパラ等の宣言-----------------------------#
+EpochNum = 100                                          
 Height = 224                                            
-Width = 224   
-Fold = 2                                                
-BatchSize = 64 
-LearningRate = 1e-8                           
-DatasetPath = r"" 
-modelPath = r""        
-ResumeModel = r""  
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")       
+Width = 224                                           
+BatchSize = 16
+Fold = 1
+LearningRate = 0.001 
+#----------------------データセット、モデルパスの宣言-------------------------#
 
+DatasetPath = r""  # データセットのパス
+valDatasetPath = r""  # 検証データセットのパス
+modelPath = r""  # モデルの保存先パス
+ResumeModel = r"" #追加学習を行うモデルのパス
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")  # 使用するデバイスの指定 (GPUが利用可能ならばGPUを使用)
+
+
+#----------------------学習用コード-------------------------#
 
 
 class Trainer():
-    def __init__(self,model, optimizer, criterion, trainLoader, valLoader, transform,epoch):
-        self.model = model
-        self.ResumeEpoch = epoch
-        self.optimizer = optimizer
-        self.criterion = criterion
-        self.trainLoader = trainLoader
-        self.valLoader = valLoader
-        self.transform = transform
-        self.totalTrainLoss = []
-        self.TrainCorrect = []
-        self.totalValLoss = []
-        self.ValCorrect = []
-        self.Fig = plt.figure(figsize=[10,10])
-    def Train(self,epoch):
-        self.model.train()
-        train_loss,train_acc = 0.0,0.0
-        t_loss,t_acc =  0.0,0.0
+    def __init__(self, model, optimizer, criterion, trainLoader, valLoader, transform, epoch):
+        self.model = model  # モデル
+        self.ResumeEpoch = epoch  # レジュームするエポック
+        self.optimizer = optimizer  # オプティマイザ
+        self.criterion = criterion  # 損失関数
+        self.trainLoader = trainLoader  # 訓練データのデータローダー
+        self.valLoader = valLoader  # 検証データのデータローダー
+        self.transform = transform  # 画像の前処理
+        self.totalTrainLoss = []  # 各エポックの訓練ロスを保存するリスト
+        self.TrainCorrect = []  # 各エポックの訓練データの正解率を保存するリスト
+        self.totalValLoss = []  # 各エポックの検証ロスを保存するリスト
+        self.ValCorrect = []  # 各エポックの検証データの正解率を保存するリスト
+        self.Fig = plt.figure(figsize=[10, 10])  # グラフのフィギュア
+
+    def Train(self, epoch):
+        self.model.train()  # モデルを訓練モードに設定
+        train_loss, train_acc = 0.0, 0.0
+        t_loss, t_acc =  0.0, 0.0
         train_log = ""
-        for batchIdx,(img,label) in enumerate(self.trainLoader):
-            img,label = Variable(img.cuda()),Variable(label.cuda())
+        for batchIdx, (img, label) in enumerate(self.trainLoader):
+            img, label = Variable(img.cuda()), Variable(label.cuda())
             output = self.model(img)
-            loss = self.criterion(output,label)
+            loss = self.criterion(output, label)
             train_loss += loss.data.item()
             self.optimizer.zero_grad()
-            loss.backward()     
+            loss.backward()
             self.optimizer.step()
             pred = output.data.max(dim=1)[1]
             train_acc += pred.eq(label.data).cpu().sum()
-            t_loss = train_loss/((batchIdx+1)*BatchSize)
-            t_acc  = 100*train_acc.data.item() / ((batchIdx+1)*BatchSize)
-            train_log = "epoch : {:3} train_loss : {:3.12} train_acc : {:3.12}".format(str(epoch+1), str(t_loss), str(t_acc))
+            t_loss = train_loss / ((batchIdx + 1) * BatchSize)
+            t_acc  = 100 * train_acc.data.item() / ((batchIdx + 1) * BatchSize)
+            train_log = "epoch : {:3} train_loss : {:3.12} train_acc : {:3.12}".format(str(epoch + 1), str(t_loss), str(t_acc))
             print("\r"+train_log,end="")
-        self.totalTrainLoss.append(t_loss)
-        self.TrainCorrect.append(t_acc)
+        self.totalTrainLoss.append(t_loss)  # エポックごとの訓練ロスを保存
+        self.TrainCorrect.append(t_acc)  # エポックごとの訓練データの正解率を保存
 
-        self.model.eval()
-        val_loss,val_acc,min_acc = 0.0,0.0,0.0
-        v_loss,v_acc = 0.0,0.0
+        self.model.eval()  # モデルを評価モードに設定
+        val_loss, val_acc, min_acc = 0.0, 0.0, 0.0
+        v_loss, v_acc = 0.0, 0.0
         val_log = ""
         with torch.no_grad():
-            for batchIdx,(img,label) in enumerate(self.valLoader):
-                img,label = Variable(img.cuda()),Variable(label.cuda())
+            for batchIdx, (img, label) in enumerate(self.valLoader):
+                img, label = Variable(img.cuda()), Variable(label.cuda())
                 output = self.model(img)
-                loss = self.criterion(output,label)
+                loss = self.criterion(output, label)
                 val_loss += loss.data.item()
                 pred = output.data.max(dim=1)[1]
                 val_acc += pred.eq(label.data).cpu().sum()
-                v_loss = val_loss/((batchIdx+1)*BatchSize)
-                v_acc = 100*val_acc.data.item() / ((batchIdx+1)*BatchSize)
+                v_loss = val_loss / ((batchIdx + 1) * BatchSize)
+                v_acc = 100 * val_acc.data.item() / ((batchIdx + 1) * BatchSize)
                 val_log = train_log + " val_loss : {:3.9} val_acc : {:3.9}".format(str(v_loss), str(v_acc))
                 print("\r"+val_log,end="")
-            self.totalValLoss.append(v_loss)
-            self.ValCorrect.append(v_acc)
+            self.totalValLoss.append(v_loss)  # エポックごとの検証ロスを保存
+            self.ValCorrect.append(v_acc)  # エポックごとの検証データの正解率を保存
         print()
         if v_acc > min_acc:
             self.max_acc = v_acc
@@ -95,30 +102,30 @@ class Trainer():
                 torch.save({'epoch': epoch,
                             'model_state_dict': model.state_dict(),
                             'optimizer_state_dict': optimizer.state_dict(),
-                            'loss': loss,},savePath)
+                            'loss': loss}, savePath)
 
     def graph_plot(self):
         plt.clf()
         plt.style.use('ggplot')
-        lossFig = self.Fig.add_subplot(2,1,1) 
+        lossFig = self.Fig.add_subplot(2, 1, 1) 
         plt.title('Loss Graph')
-        plt.plot(self.totalTrainLoss, label='train loss')
-        plt.plot(self.totalValLoss, label='validation loss')
+        plt.plot(self.totalTrainLoss, label='train loss')  # 訓練ロスをプロット
+        plt.plot(self.totalValLoss, label='validation loss')  # 検証ロスをプロット
         plt.xlabel('Epoch')
         plt.ylabel('Loss')
         plt.legend()
-        accuracyFig = self.Fig.add_subplot(2,1,2) 
+        accuracyFig = self.Fig.add_subplot(2, 1, 2) 
         plt.title('Accuracy Graph (max_Val-acc :'+str(round(float(self.max_acc),4))+'%)')
         plt.minorticks_on()
-        plt.ylim(0,100)
-        plt.plot(self.TrainCorrect, label='train acc')
-        plt.plot(self.ValCorrect, label='validation acc')
+        plt.ylim(0, 100)
+        plt.plot(self.TrainCorrect, label='train acc')  # 訓練データの正解率をプロット
+        plt.plot(self.ValCorrect, label='validation acc')  # 検証データの正解率をプロット
         plt.xlabel('Epoch')
         plt.ylabel('Accuracy')
         plt.legend()
 
     def main(self):
-        for epoch in range(self.ResumeEpoch,EpochNum):
+        for epoch in range(self.ResumeEpoch, EpochNum):
             self.Train(epoch)
             self.graph_plot()
             self.Fig.savefig("models/"+str(modelPath)+"/"+str(modelPath)+".png")
@@ -128,36 +135,35 @@ if __name__ == '__main__':
         transforms.Resize((Height, Width), interpolation=Image.BICUBIC),
         transforms.ToTensor(),
         transforms.Normalize(
-			mean=[0.5, 0.5, 0.5],
-			std=[0.5, 0.5, 0.5])
-		])
-    ss = ShuffleSplit(n_splits=Fold,test_size=0.2,random_state=0)
+            mean=[0.5, 0.5, 0.5],
+            std=[0.5, 0.5, 0.5])
+        ])
+    ss = ShuffleSplit(n_splits=Fold, test_size=0.2, random_state=0)
     dataset_labels = []
     dataset = datasets.ImageFolder(root=DatasetPath, transform=transform)
     print("Load DataSet 「"+str(DatasetPath)+"」")
     MobileNet = MobileNet_V2_Spatial()
     model = MobileNet.model.cuda()
     os.mkdir("models/"+str(modelPath))
-    for _fold,(train_index,val_index) in enumerate(ss.split(dataset)):  
+    for _fold, (train_index, val_index) in enumerate(ss.split(dataset)):  
         train_dataset = Subset(dataset, train_index)
-        val_dataset   = Subset(dataset, val_index)          
+        val_dataset = Subset(dataset, val_index)          
         trainLoader = torch.utils.data.DataLoader(train_dataset, batch_size=BatchSize, shuffle=True, num_workers=os.cpu_count(), pin_memory=True)
-        valLoader = torch.utils.data.DataLoader(val_dataset, batch_size=BatchSize, shuffle=False, num_workers=os.cpu_count(), pin_memory=True)          #シャッフルk分割
-        optimizer = optim.Adam(model.parameters(),lr=LearningRate)
+        valLoader = torch.utils.data.DataLoader(val_dataset, batch_size=BatchSize, shuffle=False, num_workers=os.cpu_count(), pin_memory=True)  # シャッフルk分割
+        optimizer = optim.Adam(model.parameters(), lr=LearningRate)
         checkpoint = torch.load(ResumeModel)
         model.load_state_dict(checkpoint['model_state_dict'])
         optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
-        #os.mkdir("Models/"+str(modelPath))
         for state in optimizer.state.values():
             for k, v in state.items():
                 if isinstance(v, torch.Tensor):
                     state[k] = v.to(device)
         epoch = checkpoint['epoch']
-        loss = checkpoint['loss']	
+        loss = checkpoint['loss']    
         criterion = nn.CrossEntropyLoss()
         model = model.to(device)
-        train_dataset = Subset(dataset,train_dataset)
-        print("Learning rate :",LearningRate)
-        print("ResumeEpoch :",epoch)
-        train = Trainer(model, optimizer, criterion,trainLoader, valLoader, transform,epoch)
+        train_dataset = Subset(dataset, train_dataset)
+        print("Learning rate:", LearningRate)
+        print("ResumeEpoch:", epoch)
+        train = Trainer(model, optimizer, criterion, trainLoader, valLoader, transform, epoch)
         train.main()
